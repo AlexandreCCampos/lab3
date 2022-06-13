04cece93934bfd0e8c881f2d966dd644cec381b5
Alexandre <alexandreccampos1@gmail.com>
2022-06-13 12:04:34 -0300

    powder DRX analysis for NaCl and KBr

---
+++ powder_spec/analise.C
@@ -0,0 +1,215 @@
+#include "TArrow.h"
+#include "TCanvas.h"
+#include "TChain.h"
+#include "TF1.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TH1D.h"
+#include "TLatex.h"
+#include "TLegend.h"
+#include "TLine.h"
+#include "TMath.h"
+#include "TMultiGraph.h"
+#include "TObjString.h"
+#include "TROOT.h"
+#include "TString.h"
+#include "TStyle.h"
+#include "TTree.h"
+#include "utility.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+TGraph read_DRX(TString path, TString filename) {
+    ifstream inp;
+
+    // getting the sample name from path
+    Ssiz_t from = 0;
+    TString sample_name, dummy;
+    filename.Tokenize(sample_name, from, ".");
+    // end getting sample name
+
+    std::vector<double> x_array, y_array;
+    double x, y, xi, h, xf;
+    int N = 0;
+
+    inp.open(path + filename);
+
+    std::string dummy1; // just to skip the line. look for other solution later
+    getline(inp, dummy1);
+    inp >> xi >> h >> xf;
+    while (inp >> x) {
+        x_array.push_back((xi + N * h) / 2);
+        y_array.push_back(x);
+        N++;
+
+        if (inp.eof()) break;
+    }
+    cout << "N = " << N << endl;
+
+    // filling histograms -make a function later
+    int nbins = x_array.size();
+    TH1D *h1 = new TH1D("h1", ";#theta (^{#circ});Contagens/#theta", nbins, xi / 2, xf / 2);
+
+    for (int k = 1; k < (x_array.size() - 1); k++) {
+        h1->SetBinContent(k, y_array.at(k));
+    }
+
+    // print_vector(&x_array);
+    // print_vector(&y_array);
+
+    inp.close();
+    // achando picos
+    vector<vector<double>> peaks = find_peaksn(&y_array, 200, 30);
+    cout << "numero de picos = " << (peaks.at(0)).size() << endl;
+
+    vector<double> theta_peaks;
+    cout << Form("Peaks (i, x, y)") << endl;
+    for (int j = 0; j < (peaks.at(0)).size(); j++) {
+        cout << Form("(%d, %f, %f)", (int)peaks.at(0).at(j), x_array[(int)peaks.at(0).at(j)], peaks.at(1).at(j)) << endl;
+
+        theta_peaks.push_back(x_array[(int)peaks.at(0).at(j)]);
+    }
+
+    int nPeaks = (peaks.at(0)).size();
+    // fim achando picos
+
+    TGraph *gr = new TGraph(x_array.size(), &x_array.at(0), &y_array.at(0));
+    TGraph *gr_peaks = new TGraph((peaks.at(0)).size(), &theta_peaks.at(0), &peaks.at(1).at(0));
+
+    gr_peaks->SetMarkerColor(kBlue);
+    gr_peaks->SetMarkerStyle(kCircle);
+
+    auto mg = new TMultiGraph();
+    mg->SetName("mg");
+    mg->SetTitle(";#theta (^{#circ});Contagens/#theta");
+
+    TCanvas *c = new TCanvas("c", " ", 0, 0, 800, 600);
+
+    mg->Add(gr);
+    mg->Add(gr_peaks, "P");
+    mg->Draw("A");
+    mg->GetYaxis()->SetMaxDigits(2);
+    mg->GetYaxis()->SetLabelSize(0.03);
+
+    c->SaveAs(Form("plots/") + filename + Form(".png"));
+    c->Close();
+
+    print_vector(&theta_peaks);
+    print_vector(&peaks.at(1));
+
+    vector<double> fit_d_peaks, fit_a_peaks;
+    double comp_k_alpha = 1.54184; // angstrom??????
+    TF1 *gaus_peak;
+    for (int k = 0; k < nPeaks; k++) {
+        gaus_peak = new TF1(Form("gauss_peak_%d", k), "gaus(0)", theta_peaks.at(k) - 0.5, theta_peaks.at(k) + 0.5);
+        gaus_peak->SetParameters(peaks.at(1).at(k), theta_peaks.at(k), 0.05);
+        // gaus_peak->FixParameter(1, theta_peaks.at(k));
+
+        h1->Fit(Form("gauss_peak_%d", k), "R+");
+        fit_d_peaks.push_back(comp_k_alpha * 100 / (2 * sin(gaus_peak->GetParameter(1) * M_PI / 180)));
+        fit_a_peaks.push_back(0);
+
+        cout << "chi2/ndof = " << gaus_peak->GetChisquare() << "/" << gaus_peak->GetNDF() << " = " << gaus_peak->GetChisquare() / gaus_peak->GetNDF() << endl;
+        double beta = (M_PI / 2) * 2.355 * gaus_peak->GetParameter(2);
+        double d_cristallite = (4. / 3) * 0.9 * comp_k_alpha * 100 / (beta * cos(gaus_peak->GetParameter(1) * M_PI / 180));
+        cout << Form("cristallite diameter:  d = %f pm", d_cristallite) << endl;
+    }
+
+    plot_hist(h1, Form("plots/h_") + sample_name);
+
+    TCanvas *c2 = new TCanvas("c2", "", 800, 600);
+    c2->SetGrid();
+
+    c2->cd();
+    vector<TF1 *> net_par;
+    TF1 *linear_hkl;
+    TLatex *leg;
+    vector<TLatex *> leg_array;
+
+    vector<int> gambiarra_filter_miller_indices;
+
+    // cout << "(hkl)" << endl;
+    for (int h = 0; h < 5; h++) {
+        for (int k = 0; k < 5; k++) {
+            for (int l = 1; l < 5; l++) {
+                if (vector_contain(h * h + k * k + l * l, &gambiarra_filter_miller_indices)) {
+                    linear_hkl = new TF1(Form("linear_%d%d%d", h, k, l), "[0]*x", 0, 340);
+                    linear_hkl->SetParameter(0, sqrt(h * h + k * k + l * l));
+                    linear_hkl->SetNpx(100000);
+                    linear_hkl->SetLineColor(h * h + k * k + l * l);
+                    linear_hkl->SetLineWidth(1);
+                    linear_hkl->SetTitle(";d (pm);a (pm)");
+
+                    leg = new TLatex(345, linear_hkl->Eval(345), Form("(%d%d%d)", h, k, l));
+                    leg->SetTextSize(0.015);
+                    leg->SetTextAlign(13);
+                    leg_array.push_back(leg);
+                    // cout << Form("(%d%d%d)", h, k, l) << endl;
+                    net_par.push_back(linear_hkl);
+                    gambiarra_filter_miller_indices.push_back(h * h + k * k + l * l);
+                }
+            }
+        }
+    }
+
+    TGraph *peaks_from_fit = new TGraph(fit_d_peaks.size(), &fit_d_peaks.at(0), &fit_a_peaks.at(0));
+    peaks_from_fit->SetMarkerStyle(20);
+    TLine *vertical_hkl;
+    vector<TLine *> vertical_par;
+    for (int k = 0; k < fit_d_peaks.size(); k++) {
+        vertical_hkl = new TLine(fit_d_peaks.at(k), 0, fit_d_peaks.at(k), 1000);
+        vertical_par.push_back(vertical_hkl);
+    }
+
+    TLine *horizontal_hkl;
+    vector<TLine *> horizontal_par;
+    for (int k = 0; k < 8; k++) {
+        double y = net_par.at(k)->Eval(fit_d_peaks.at(1));
+        horizontal_hkl = new TLine(60, y, 340, y);
+        horizontal_par.push_back(horizontal_hkl);
+    }
+
+    net_par.at(net_par.size() - 1)->Draw();
+    leg_array.at(net_par.size() - 1)->Draw("SAME");
+    for (int k = 0; k < net_par.size() - 1; k++) {
+        net_par.at(k)->Draw("SAME");
+        leg_array.at(k)->Draw("SAME");
+    }
+    peaks_from_fit->Draw("SAMEP");
+    print_vector(&fit_d_peaks);
+    for (int k = 0; k < vertical_par.size(); k++) {
+        vertical_par.at(k)->Draw("SAME");
+    }
+
+    for (int k = 0; k < horizontal_par.size(); k++) {
+        horizontal_par.at(k)->Draw("SAME");
+    }
+
+    c2->SaveAs(Form("plots/indices_") + filename + Form(".png"));
+    c2->Close();
+
+    // calculating 2nd and 3rd order peaks
+    cout << "------ second and third orders ------" << endl;
+    cout << "1st \t\t 2nd \t\t 3rd" << endl;
+    for (int k = 0; k < fit_d_peaks.size(); k++) {
+        double ang1, ang2, ang3;
+        ang1 = asin(comp_k_alpha * 100 / (2 * fit_d_peaks.at(k))) * 180 / M_PI;
+        ang2 = asin(2 * comp_k_alpha * 100 / (2 * fit_d_peaks.at(k))) * 180 / M_PI;
+        ang3 = asin(3 * comp_k_alpha * 100 / (2 * fit_d_peaks.at(k))) * 180 / M_PI;
+
+        cout << Form("%f \t %f \t %f", ang1, ang2, ang3) << endl;
+    }
+
+    delete h1;
+    return *gr_peaks;
+}
+
+void analise() {
+    read_DRX("data/", "KBr.dat");
+    // read_DRX("data/", "KBrdet62.dat");
+    read_DRX("data/", "NaClbrutoPlano.dat");
+    read_DRX("data/", "NaClmoido.dat");
+}
---
+++ powder_spec/utility.h
@@ -0,0 +1,213 @@
+#pragma once
+
+#include "TArrow.h"
+#include "TCanvas.h"
+#include "TChain.h"
+#include "TF1.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TH1D.h"
+#include "TLatex.h"
+#include "TLegend.h"
+#include "TMath.h"
+#include "TMultiGraph.h"
+#include "TObjString.h"
+#include "TROOT.h"
+#include "TString.h"
+#include "TStyle.h"
+#include "TTree.h"
+#include "utility.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+vector<vector<T>> find_peaksn(vector<T> *x, double height, int order = 1) {
+    int peak_found = 0;
+    vector<vector<double>> peaks;
+    vector<double> peak_x, peak_y;
+    bool der_inf = true, der_sup = true;
+    for (int i = order; i < (x->size() - order); i++) {
+        for (int j = 1; j < (order + 1); j++) {
+            der_inf = der_inf && (x->at(i) > x->at(i - j));
+            der_sup = der_sup && (x->at(i) > x->at(i + j));
+        }
+        if (der_inf && der_sup && x->at(i) >= height) {
+            peak_found++;
+            peak_x.push_back(i);
+            peak_y.push_back(x->at(i));
+        }
+        der_inf = true;
+        der_sup = true;
+    }
+    peaks.push_back(peak_x);
+    peaks.push_back(peak_y);
+    return peaks;
+}
+
+void plot_hist(TH1 *h, TString output_name = "") {
+    TCanvas *c = new TCanvas("c", "", 800, 600);
+    h->Draw();
+    c->SaveAs(output_name + Form(".png"));
+    c->Close();
+}
+
+template <typename T>
+void print_vector(vector<T> *v) {
+    cout << "v = [";
+    for (int k = 0; k < v->size() - 1; k++) {
+        cout << Form("%f,", v->at(k));
+        if (k == v->size() - 2) cout << Form("%f]", v->at(k + 1)) << endl;
+    }
+}
+
+// factorial of n
+unsigned int fact(unsigned int n) {
+    unsigned int ret = 1;
+    for (unsigned int i = 1; i <= n; ++i) {
+        ret *= i;
+    }
+    return ret;
+}
+
+// statistic from a vector - return a vector with results: mean, stddev, variance, min value and max value
+std::vector<double> get_stat(std::vector<double> *sample, TString sample_name = "", bool print = 1) {
+
+    int N = 0;
+    double mean, stddev, variance, x_max = 0, x_min = 1.e6, sum1 = 0, sum2 = 0; // this values for max and min varies to each sample
+
+    for (int i = 0; i < sample->size(); i++) {
+        N++;
+        x_max = max(sample->at(i), x_max);
+        x_min = min(sample->at(i), x_min);
+        sum1 += sample->at(i);
+        sum2 += sample->at(i) * sample->at(i);
+    }
+
+    mean = sum1 / N;
+    variance = sum2 / N - ((sum1 / N) * (sum1 / N));
+    stddev = sqrt(variance);
+
+    if (print) {
+        // N must be equal to sample->size() and final i so
+        cout << "---------------------------------------------" << endl;
+        cout << "estat for " << sample_name << endl;
+        cout << "N = " << N << endl;
+        cout << "sample->size() = " << sample->size() << endl;
+        cout << "mean = " << mean << endl;
+        cout << "stddev = " << stddev << endl;
+        cout << "variance = " << variance << endl;
+        cout << "x_max = " << x_max << endl;
+        cout << "x_min = " << x_min << endl;
+        cout << "---------------------------------------------" << endl;
+    }
+
+    std::vector<double> results;
+    results.push_back(mean);
+    results.push_back(stddev);
+    results.push_back(variance);
+    results.push_back(x_min);
+    results.push_back(x_max);
+
+    return results;
+}
+
+void read_TGraph(TString path, TString output_path = "plots/") {
+    ifstream inp;
+
+    std::vector<double> x_array, y_array;
+    double x;
+    int N = 0;
+
+    cout << "opening file: " << path << endl;
+    inp.open(path);
+
+    while (inp >> x) {
+
+        x_array.push_back(N);
+        y_array.push_back(x);
+        N++;
+        if (inp.eof()) break;
+    }
+
+    inp.close();
+    TGraph *gr_peaks = new TGraph(x_array.size(), &x_array.at(0), &y_array.at(0));
+
+    Ssiz_t from = 0;
+    TString sample_name, dummy;
+    path.Tokenize(dummy, from, "/");
+    path.Tokenize(dummy, from, "/");
+    // path.Tokenize(dummy, from, "/");
+    from = 0;
+    dummy.Tokenize(sample_name, from, ".");
+
+    TCanvas *c = new TCanvas("c1", "", 800, 600);
+    gr_peaks->Draw();
+    c->SaveAs(output_path + sample_name + Form(".png"));
+    c->Close();
+}
+
+template <typename T, typename U>
+void plot_TGraph_multifunction(U *f, vector<T *> *mf, TString title = "", TString draw_opt = "", TString filename = "", double leg_pos_x = 0, double leg_pos_y = 0) { // title must have same pattern of TH1 constructor => "name; axisx; axisy"
+    TCanvas *c = new TCanvas("c", "", 800, 600);
+    c->SetGrid();
+    gStyle->SetOptFit(0);
+    // gStyle->SetMaxDigitsY(2);
+    f->GetYaxis()->SetMaxDigits(2);
+    f->GetYaxis()->SetLabelSize(0.03);
+    // getting names
+    Ssiz_t from = 0;
+    TString gtitle, yaxistitle, xaxistitle;
+    title.Tokenize(gtitle, from, ";");
+    title.Tokenize(xaxistitle, from, ";");
+    title.Tokenize(yaxistitle, from, ";");
+    f->SetTitle(gtitle);
+    f->GetXaxis()->SetTitle(xaxistitle);
+    f->GetYaxis()->SetTitle(yaxistitle);
+    // f->SetMarkerColor(4);
+    f->SetMarkerSize(0.5);
+    f->SetMarkerStyle(20);
+    // f->SetMarkerSize(1.2);
+    // f->SetMarkerStyle(4);
+    // f->SetMarkerColor(kBlue);
+    f->Draw(draw_opt);
+
+    TLatex *text = new TLatex(leg_pos_x, leg_pos_y, Form("y = ax + b"));
+    TLatex *text1;
+    TLatex *text2;
+    text->SetTextSize(0.05);
+
+    if (mf != NULL) {
+        for (int i = 0; i < mf->size(); i++) {
+            (mf->at(i))->Draw("P same");
+        }
+
+        if (leg_pos_x != 0 && leg_pos_y != 0) {
+            TF1 *f1 = f->GetFunction("fitlinear");
+            text1 = new TLatex(leg_pos_x, leg_pos_y - (leg_pos_y * 0.1), Form("a = (%.3f #pm %.3f)", f1->GetParameter(0), f1->GetParError(0)));
+            text2 = new TLatex(leg_pos_x, leg_pos_y - 2 * (leg_pos_y * 0.1), Form("b = (%.1f #pm %.1f)", f1->GetParameter(1), f1->GetParError(1)));
+            text1->SetTextSize(0.05);
+            text2->SetTextSize(0.05);
+            text->Draw("same");
+            text1->Draw("same");
+            text2->Draw("same");
+        }
+    }
+
+    c->SaveAs(filename + Form(".png"));
+    // c->SaveAs(filename + Form(".root"));
+    //  c->SaveAs(filename + Form(".C"));
+
+    c->Close();
+}
+
+bool vector_contain(int a, vector<int> *v) {
+    bool t = true;
+    for (int k = 0; k < v->size(); k++) {
+        if (a == v->at(k)) t = false;
+    }
+    // cout << "t = " << t << endl;
+    return t;
+}
