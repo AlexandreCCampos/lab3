2c6274e2b7b3501e57a164e3daf3f642133ea786
Alexandre Campos <alexandreccampos1@gmail.com>
2022-07-14 13:10:17 -0300

    update hall-effect

--- hall_effect/analise.C
+++ hall_effect/analise.C
@@ -53,7 +53,7 @@ void plot_TGraph_multifunction(U *f, vector<T *> *mf, TString title = "", TStrin
     c->Close();
 }
 
-void make_fit(TString file_name = "data/dados_calibracao_Hall.txt") {
+void make_fit_magnet_calibration(TString file_name = "data/dados_calibracao_Hall.txt", TString h_title = "") {
     // reading a dat file with name in file_name variable
     ifstream inp;
     inp.open(file_name);
@@ -91,9 +91,57 @@ void make_fit(TString file_name = "data/dados_calibracao_Hall.txt") {
     file_name.Tokenize(path, from, "/");
     file_name.Tokenize(filename, from, "/");
 
-    plot_TGraph_multifunction(graph1, &functions, ";I (A); B (mT)", "AP*", Form("plots/") + filename);
+    plot_TGraph_multifunction(graph1, &functions, h_title, "AP*", Form("plots/") + filename);
+}
+
+void make_fit(TString file_name = "data/dados_calibracao_Hall.txt", TString h_title = "") {
+    // reading a dat file with name in file_name variable
+    ifstream inp;
+    inp.open(file_name);
+    double _x, _y, xmax, xmin;
+    vector<double> x, y;
+
+    std::string dummy1; // just to skip the line. look for other solution later
+    getline(inp, dummy1);
+
+    while (inp >> _y >> _x) {
+        _x = _x * 10;
+        if (_x > xmax) xmax = _x;
+        if (_x < xmin) xmin = _x;
+
+        x.push_back(_x);
+        y.push_back(_y);
+    }
+
+    for (int i = 0; i < x.size(); i++) {
+        x.at(i) = x.at(i);
+        y.at(i) = y.at(i);
+    }
+
+    double offset = (xmax - xmin) * 0.1;
+
+    TF1 *linfac = new TF1("linfac", "[0]*x+[1]", xmin - offset, xmax + offset);
+    linfac->SetNpx(10000);
+    TGraph *graph1 = new TGraph(x.size(), &x.at(0), &y.at(0));
+
+    graph1->Fit("linfac", "R");
+
+    vector<TF1 *> functions;
+    functions.push_back(linfac);
+
+    // getting names
+    Ssiz_t from = 0;
+    TString path, filename;
+    file_name.Tokenize(path, from, "/");
+    file_name.Tokenize(filename, from, "/");
+
+    plot_TGraph_multifunction(graph1, &functions, h_title, "AP*", Form("plots/") + filename);
 }
 
 void analise() {
-    make_fit("data/dados_calibracao_Hall.txt");
+    // make_fit_magnet_calibration("data/dados_calibracao_Hall.txt", ";I (A); B (mT)");
+    make_fit("data/zinc_0.25A.txt", ";I (mA); U_{H} (mV)");
+    make_fit("data/zinc_0.5A.txt", ";I (mA); U_{H} (mV)");
+    make_fit("data/zinc_0.75A.txt", ";I (mA); U_{H} (mV)");
+    make_fit("data/zinc_1A.txt", ";I (mA); U_{H} (mV)");
 }
